a99a4bdf5ce53ac379cb33916494e17d25f05edc
duyuqian <809009017@qq.com>
2020-08-03 10:35:55 +0800

    [duyuqian] feat: return none when given none

---
+++ src/main/java/Mommifier.java
@@ -0,0 +1,6 @@
+public class Mommifier {
+
+    public String convertString(String s) {
+        return "";
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class MommifierTest {
+    @Test
+    void should_return_none_when_given_none() {
+        //given
+        String stringOfNone="";
+        //when
+        Mommifier mommifier=new Mommifier();
+        String result = mommifier.convertString(stringOfNone);
+        //then
+        String expected = "";
+        assertEquals(result, expected);
+    }
 }
