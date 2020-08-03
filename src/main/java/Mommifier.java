f617708357dbb9eaf2b9979a24c80f1641314f9a
duyuqian <809009017@qq.com>
2020-08-03 12:32:50 +0800

    [duyuqian] feat: insert mommy when given strings with repeat vowels

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -1,26 +1,47 @@
 public class Mommifier {
+    public static char charA = 'a';
+    public static char charE = 'e';
+    public static char charI = 'i';
+    public static char charO = 'o';
+    public static char charU = 'u';
+    public static String doubleA = "aa";
+    public static String doubleE = "ee";
+    public static String doubleI = "ii";
+    public static String doubleO = "oo";
+    public static String doubleU = "uu";
+
 
     public String convertString(String s) {
-        if (s != null ){
-            if (hasRepeatVowels(s)){
+        if (s != null) {
+            if (hasRepeatVowels(s)) {
                 return insertMommy(s);
-            }else {
+            } else {
                 return s;
             }
-        }else {
+        } else {
             throw new RuntimeException();
         }
     }
 
-    public boolean hasRepeatVowels(String s){
-        boolean result=false;
-        if (s.contains("aa")||s.contains("ee")||s.contains("ii")||s.contains("oo")||s.contains("uu")){
-            result=true;
+    public boolean hasRepeatVowels(String s) {
+        boolean result = false;
+        if (s.contains(doubleA) || s.contains(doubleE) || s.contains(doubleI) || s.contains(doubleO) || s.contains(doubleU)) {
+            result = true;
         }
         return result;
     }
 
-    public String insertMommy(String s){
-        return "";
+    public String insertMommy(String s) {
+        String result = s;
+        for (int i = 1; i < s.length(); i++) {
+            if ((s.charAt(i) == charA || s.charAt(i) == charE || s.charAt(i) == charI || s.charAt(i) == charO || s.charAt(i) == charU) && s.charAt(i) == s.charAt(i - 1)) {
+                result = s.substring(0, i) + "mommy" + s.substring(i);
+                break;
+            }
+        }
+        if (hasRepeatVowels(result)) {
+            return insertMommy(result);
+        }
+        return result;
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -6,35 +6,36 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class MommifierTest {
     @Test
     void should_return_none_when_given_none() {
-        //given
-        String stringOfNone="";
-        //when
-        Mommifier mommifier=new Mommifier();
+        String stringOfNone = "";
+        Mommifier mommifier = new Mommifier();
         String result = mommifier.convertString(stringOfNone);
-        //then
         String expected = "";
         assertEquals(result, expected);
     }
+
     @Test
     void should_throw_exception_when_given_null() {
-        //given
-        String stringOfNull=null;
-        //when
-        Mommifier mommifier=new Mommifier();
-        //then
-        assertThrows(RuntimeException.class,  ()-> {
-            mommifier.convertString(stringOfNull);
+        Mommifier mommifier = new Mommifier();
+        assertThrows(RuntimeException.class, () -> {
+            mommifier.convertString(null);
         });
     }
+
     @Test
     void should_return_itself_when_given_strings_with_no_repeat_vowels() {
-        //given
-        String stringOfNoVowels="abcde";
-        //when
-        Mommifier mommifier=new Mommifier();
-        String result = mommifier.convertString(stringOfNoVowels);
-        //then
-        String expected ="abcde";
+        String stringOfNoRepeatVowels = "abcde";
+        Mommifier mommifier = new Mommifier();
+        String result = mommifier.convertString(stringOfNoRepeatVowels);
+        String expected = "abcde";
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void should_insert_mommy_when_given_strings_with_repeat_vowels() {
+        String stringOfRepeatVowels = "aabcdee";
+        Mommifier mommifier = new Mommifier();
+        String result = mommifier.convertString(stringOfRepeatVowels);
+        String expected = "amommyabcdemommye";
         assertEquals(result, expected);
     }
 }
